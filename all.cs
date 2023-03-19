6e04cb6023c85acc2ac80413fe492e23cf438a38
Sergey <114340735+rebelly@users.noreply.github.com>
2023-03-19 21:42:22 +0300

    Update all.cs

--- all.cs
+++ all.cs
@@ -1,44 +1,79 @@
-	using System; 
-	class  point3d{
-		public int x;
-		public int y;
-		public int z;
-		public point3d(){
-			x = 0;
-			y= 0;
-			z = 0;
-			}
-		public point3d(int x1, int y1, int z1){
-			x = x1;
-			y = y1;
-			z = z1;
-		}
-		public void move(char axis, int dist){
-			if (axis == 'x'){
-				x += dist;
-				}
-			if (axis == 'y'){
-				y += dist;
-				}
-			if (axis == 'z'){
-				z += dist;
-				}
-			}
-		public void showpos(){
-			Console.WriteLine($"Вы находитесь на координатах: x: {x} y:{y} z:{z}");
-			}
+using System;
+class point3d
+{
+	public int x;
+	public int y;
+	public int z;
+	public point3d()
+	{
+		x = 0;
+		y = 0;
+		z = 0;
 	}
-	class Program
+	public point3d(int x1, int y1, int z1)
+	{
+		x = x1;
+		y = y1;
+		z = z1;
+	}
+	public void move(char axis, int dist)
+	{
+		if (axis == 'x')
+		{
+			x += dist;
+		}
+		if (axis == 'y')
+		{
+			y += dist;
+		}
+		if (axis == 'z')
+		{
+			z += dist;
+		}
+	}
+	public void len_rad_vec()
+    {
+		Console.WriteLine($"Длина радиус-вектора, считая от начала координат {Math.Sqrt(x*x+y*y+z*z)}");
+    }
+	public void showpos()
+	{
+		Console.WriteLine($"Объект  находитесь на координатах: x: {x} y:{y} z:{z}");
+	}
+}
+class Program
 {
 
-	
-    public static void Main()
-    {
-		point3d pos = new point3d();
-		point3d pos1 = new point3d(1,2,3);
-		pos1.showpos();
-		pos1.move('x', 14);
-		pos1.showpos();
-    }
-}
 
+	public static void Main()
+	{
+		int a;
+		Console.WriteLine("Введите 0, если хотите создать объект в нулевых координатах, и любое другое число , чтобы ввести конкретные координаты");
+		a = int.Parse(Console.ReadLine());
+		point3d pos1 = new point3d();
+		if (a == 0)
+			{ 
+			pos1 = new point3d(); 
+		}
+		else
+			{
+			Console.Write("Введите координаты по оси х");
+			int x1 = int.Parse(Console.ReadLine());
+			Console.Write("\n Введите координаты по оси y");
+			int y1 = int.Parse(Console.ReadLine());
+			Console.Write("\n Введите координаты по оси z");
+			int z1 = int.Parse(Console.ReadLine());
+			pos1 = new point3d(x1, y1, z1); 
+		}
+		pos1.showpos();
+		Console.WriteLine("Введите ось, по которой нужно подвинуть объект");
+		char b = Char.Parse(Console.ReadLine());
+		while (!"xyz".Contains(b))
+			Console.WriteLine("Ось может только: x y или z");
+			 b = Char.Parse(Console.ReadLine());
+		Console.WriteLine("Введите насколько надо подвинуть объект");
+		int c = int.Parse(Console.ReadLine());
+		pos1.move(b, c);
+		pos1.showpos();
+		pos1.len_rad_vec();
+	}
+}
