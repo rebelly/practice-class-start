fa972971603b6394f9e73470c3a4e58435d1cf5e
Sergey <114340735+rebelly@users.noreply.github.com>
2023-03-27 19:17:22 +0300

    Update all.cs

--- all.cs
+++ all.cs
@@ -6,15 +6,28 @@ class point3d
 	public int z;
 	public point3d()
 	{
-		x = 0;
-		y = 0;
-		z = 0;
+		this.x = 0;
+		this.y = 0;
+		this.z = 0;
 	}
 	public point3d(int x1, int y1, int z1)
 	{
-		x = x1;
-		y = y1;
-		z = z1;
+		this.x = x1;
+		this.y = y1;
+		this.z = z1;
+	}
+	public point3d(decimal x1)
+	{
+		int j = 1;
+		this.x = (int) x1;
+		int y = 0;
+		while (x1 > 1)
+        {
+			j++;	
+        }
+		x1 *= (decimal)Math.Pow(10, j);
+		this.y = (int)x1;
+		this.z = 0;
 	}
 	public void move(char axis, int dist)
 	{
@@ -31,14 +44,74 @@ class point3d
 			z += dist;
 		}
 	}
-	public double len_rad_vec()
-    {
-		Console.WriteLine($"Длина радиус-вектора, считая от начала координат {Math.Sqrt(x*x+y*y+z*z)}");
-    }
-	public void showpos()
+	public string len_rad_vec()
 	{
-		Console.WriteLine($"Объект  находитесь на координатах: x: {x} y:{y} z:{z}");
+		return $"Длина радиус-вектора, считая от начала координат {Math.Sqrt(x * x + y * y + z * z)}";
 	}
+	public string showpos()
+	{
+		return $"Объект  находитесь на координатах: x: {x} y:{y} z:{z}";
+	}
+	public int X
+	{
+		set
+		{
+			if (value > 0)
+			{
+				x = value;
+			}
+			else
+			{
+				Console.WriteLine("Х должен принимать положительные значения ");
+			}
+		}
+	}
+	public int Y
+	{
+		set
+		{
+			if (value > 0 && value <= 100)
+			{
+				y = value;
+			}
+			else if (value > 0 && value > 100)
+			{
+				y = 100;
+			}
+			else
+			{
+				Console.WriteLine("Y должен принимать положительные значения, максимум - 100 ");
+			}
+		}
+	}
+	public int Z
+	{
+		set
+		{
+			if (value <= x+y )
+			{
+				z = value;
+			}
+			else
+			{
+				Console.WriteLine($"Z должен принимать значения, меньшие чем сумма двумерных координат, в нашем случае это {x+y} ");
+			}
+		}
+	}
+	public bool inzone
+    {
+		get
+        {
+			if (x >= 10)
+            {
+				if (y >= 2)
+                {
+					if (x <= y) { return true;  }
+                }
+            }
+			return false;
+        }
+    }
 }
 class Program
 {
@@ -47,33 +120,72 @@ class Program
 	public static void Main()
 	{
 		int a;
-		Console.WriteLine("Введите 0, если хотите создать объект в нулевых координатах, и любое другое число , чтобы ввести конкретные координаты");
+		Console.WriteLine("Введите 0, если хотите создать объект в нулевых координатах, 1, если хотите указать х и у, и любое другое число , чтобы ввести конкретные координаты");
 		a = int.Parse(Console.ReadLine());
 		point3d pos1;
 		if (a == 0)
-			{ 
-			pos1 = new point3d(); 
+		{
+			pos1 = new point3d();
+		}
+		else if (a == 1)
+        {
+			Console.WriteLine("Введите число с точкой, дробная часть которого равна координатам Y, а целая - X");
+			decimal x = decimal.Parse(Console.ReadLine());
+			pos1 = new point3d(x);
 		}
 		else
-			{
+		{
 			Console.Write("Введите координаты по оси х");
 			int x1 = int.Parse(Console.ReadLine());
 			Console.Write("\n Введите координаты по оси y");
 			int y1 = int.Parse(Console.ReadLine());
 			Console.Write("\n Введите координаты по оси z");
 			int z1 = int.Parse(Console.ReadLine());
-			pos1 = new point3d(x1, y1, z1); 
+			pos1 = new point3d(x1, y1, z1);
 		}
-		pos1.showpos();
-		Console.WriteLine("Введите ось, по которой нужно подвинуть объект");
-		char b = Char.Parse(Console.ReadLine());
-		while (!"xyz".Contains(b))
-			Console.WriteLine("Ось может только: x y или z");
-			 b = Char.Parse(Console.ReadLine());
-		Console.WriteLine("Введите насколько надо подвинуть объект");
-		int c = int.Parse(Console.ReadLine());
-		pos1.move(b, c);
-		pos1.showpos();
-		pos1.len_rad_vec();
+		int req = int.Parse(Console.ReadLine());
+		while (req != 0)
+		{
+			switch (req)
+            {
+				case 1:
+					Console.WriteLine("Введите ось, по которой нужно подвинуть объект");
+					char b = Char.Parse(Console.ReadLine());
+					while (!"xyz".Contains(b))
+						Console.WriteLine("Ось может только: x y или z");
+					b = Char.Parse(Console.ReadLine());
+					Console.WriteLine("Введите насколько надо подвинуть объект");
+					int c = int.Parse(Console.ReadLine());
+					pos1.move(b, c);
+					break;
+				case 2:
+					Console.WriteLine(pos1.len_rad_vec());
+					break;
+				case 3:
+					Console.WriteLine("Введите , чему теперь равен Х");
+					pos1.X = int.Parse(Console.ReadLine());
+					break;
+				case 4:
+					Console.WriteLine("Введите , чему теперь равен Y");
+					pos1.Y = int.Parse(Console.ReadLine());
+					break;
+				case 5:
+					Console.WriteLine("Введите , чему теперь равен Z");
+					pos1.Z = int.Parse(Console.ReadLine());
+					break;
+				case 6:
+					if (pos1.inzone)
+					Console.WriteLine("Точка находится в зоне, ограниченной треугольником:");
+                    else Console.WriteLine("Точка не находится в зоне, ограниченной треугольником:");
+					break;
+				case 7:
+
+					break;
+			}
+			pos1.showpos();
+
+			pos1.showpos();
+			
+		}
 	}
 }
